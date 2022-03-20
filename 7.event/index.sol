c2a3bf57b0fdedf11251a2633be69df478d48bc7
xudan <xudan@apowo.com>
2022-03-20 23:53:44 +0800

    Event

--- 7.event/index.sol
+++ 7.event/index.sol
@@ -1,3 +1,13 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.4;
+
+contract Event{
+    event Log(address sender, string message);
+
+    function transfer() public {
+        emit Log(msg.sender, "I send 1 ether to you");
+        emit Log(msg.sender, "I send 2 ether to you");
+        emit Log(msg.sender, "I send 3 ether to you");
+    }
+}
