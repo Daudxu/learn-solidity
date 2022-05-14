35788bcfc1a12ed56d14b2fa47f2167db4aade34
xudan <875126243@qq.com>
2022-05-14 17:09:34 +0800

    募捐合约

--- 14.Raise money 2/demo1.sol
+++ 14.Raise money 2/demo1.sol
@@ -1,12 +1,15 @@
-// SPDX-License-Identifier: GPL-3.0
-
 pragma solidity ^0.4.25;
 
-contract Demo {
+contract Donation {
+    struct DonorInfo {
+        address[] donors;
+        mapping(address => uint256) ledger;
+    }
+    mapping(address => DonorInfo) DonationHistory;
     event LogDonate(
         address streamer,
         address donor,
-        string nikename,
+        string nickname,
         uint256 value,
         string message
     );
@@ -16,7 +19,36 @@ contract Demo {
         string _nickname,
         string _message
     ) public payable {
+        require(msg.value > 0);
+
         _streamer.transfer(msg.value);
+
+        if (DonationHistory[_streamer].ledger[msg.sender] == 0) {
+            DonationHistory[_streamer].donors.push(msg.sender);
+        }
+
+        DonationHistory[_streamer].ledger[msg.sender] += msg.value;
         emit LogDonate(_streamer, msg.sender, _nickname, msg.value, _message);
+    }
+
+    function getDonorList() public view returns (address[]) {
+        return DonationHistory[msg.sender].donors;
+    }
+
+    event LogListDonorInfo(address streamer, address user, uint256 value);
+
+    function listDonorInfo() public {
+        for (
+            uint256 i = 0;
+            i < DonationHistory[msg.sender].donors.length;
+            i++
+        ) {
+            address user = DonationHistory[msg.sender].donors[i];
+            emit LogListDonorInfo(
+                msg.sender,
+                user,
+                DonationHistory[msg.sender].ledger[user]
+            );
+        }
     }
 }
