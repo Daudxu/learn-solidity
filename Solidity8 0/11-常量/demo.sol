82f2b3a5d7d7acb875798bb6c34da925a570845c
xudan <875126243@qq.com>
2022-05-17 15:30:20 +0800

    Constants

---
+++ Solidity8 0/11-常量/demo.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Constants {
+    address public constant MY_ADDRESS =
+        0x1537f0d523a264d3bBDf8d4A4e8778cd65b6D166;
+    uint256 public constant MY_UINT = 123;
+}
+
+contract Var {
+    address public MY_ADDRESS = 0x1537f0d523a264d3bBDf8d4A4e8778cd65b6D166;
+}
