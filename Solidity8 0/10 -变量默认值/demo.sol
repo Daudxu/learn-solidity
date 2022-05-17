cee0a78bec3d2404d464810832403738adcca8f0
xudan <875126243@qq.com>
2022-05-17 15:12:19 +0800

    DefVar

---
+++ Solidity8 0/10 -变量默认值/demo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract DefVar {
+    bool public b;
+    uint256 public u;
+    int256 public i;
+    address public a;
+    bytes32 public b32;
+}
