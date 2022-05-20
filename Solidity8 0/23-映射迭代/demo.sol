4291913d6a71eb69da85087b0b7925d59b2adc63
xudan <875126243@qq.com>
2022-05-20 15:49:40 +0800

    mapping iteration

---
+++ Solidity8 0/23-映射迭代/demo.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract Mapping {
+    mapping(address => uint256) public balances;
+    mapping(address => bool) public inserted;
+    address[] public keys;
+
+    function set(address _key, uint256 _val) external {
+        inserted[_key] = _val;
+
+        if (!inserted[_key]) {
+            inserted[_key] = true;
+            keys.push(_key);
+        }
+    }
+
+    function getSize() external view returns (uint256) {
+        return keys.length;
+    }
+
+    function first() external view returns (uint256) {
+        return balances[keys[0]];
+    }
+
+    function last() external view returns (uint256) {
+        return balances[keys[keys.length - 1]];
+    }
+
+    function get(uint256 _i) external view returns (uint256) {
+        return balances[keys[_i]];
+    }
+}
