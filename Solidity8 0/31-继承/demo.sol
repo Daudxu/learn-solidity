6a1a776e9493ffe943ab16c72395e357b5b47750
xudan <875126243@qq.com>
2022-05-23 09:43:34 +0800

    is

---
+++ Solidity8 0/31-继承/demo.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract A {
+    function foo() public pure virtual returns (string memory) {
+        return "A";
+    }
+
+    function bar() public pure virtual returns (string memory) {
+        return "A";
+    }
+
+    function baz() public pure returns (string memory) {
+        return "A";
+    }
+}
+
+contract B is A {
+    function foo() public pure override returns (string memory) {
+        return "B";
+    }
+
+    function bar() public pure override returns (string memory) {
+        return "B";
+    }
+}
