4c5ceea7616106f45db28adf9cf96c06e3f06c8e
xudan <875126243@qq.com>
2022-05-25 19:21:28 +0800

    interface

--- Solidity8 0/42-接口合约/demo.sol
+++ Solidity8 0/42-接口合约/demo.sol
@@ -2,67 +2,29 @@
 
 pragma solidity ^0.8.3;
 
-// start  gas
-// use calldata
-// load state variables to memory
-// short circuit
-// loop increments
-// cache array length
-// load array elements to memory
+interface ICounter {
+    function count() external view returns (uint256);
 
-contract gasdemo {
-    uint256 public total;
+    function inc() external;
+}
 
-    function ex(uint256[] calldata nums) external {
-        for (uint256 i = 0; i < nums.length; i += 1) {
-            bool isEven = nums[i] % 2 == 0;
-            bool isLessThan99 = nums[i] < 99;
+contract demo {
+    uint256 public count;
 
-            if (isEven && isLessThan99) {
-                total += nums[i];
-            }
-        }
+    function ICounter(address _counter) external {
+        ICounter(_counter).inc();
+        count = ICounter(_counter).count();
+    }
+}
+
+contract Counter {
+    uint256 public count;
+
+    function inc() external {
+        count += 1;
     }
 
-    function ex1(uint256[] calldata nums) external {
-        uint256 _tatal = total;
-        for (uint256 i = 0; i < nums.length; i += 1) {
-            bool isEven = nums[i] % 2 == 0;
-            bool isLessThan99 = nums[i] < 99;
-
-            if (isEven && isLessThan99) {
-                _tatal += nums[i];
-            }
-        }
-    }
-
-    function ex2(uint256[] calldata nums) external {
-        uint256 _tatal = total;
-        for (uint256 i = 0; i < nums.length; i += 1) {
-            if (nums[i] % 2 == 0 && nums[i] < 99) {
-                _tatal += nums[i];
-            }
-        }
-    }
-
-    function ex3(uint256[] calldata nums) external {
-        uint256 _tatal = total;
-        for (uint256 i = 0; i < nums.length; ++i) {
-            if (nums[i] % 2 == 0 && nums[i] < 99) {
-                _tatal += nums[i];
-            }
-        }
-    }
-
-    function ex4(uint256[] calldata nums) external {
-        uint256 _tatal = total;
-        uint256 len = nums.length;
-        for (uint256 i = 0; i < len; ++i) {
-            uint256 num = nums[i];
-            if (num % 2 == 0 && num < 99) {
-                _tatal += num;
-            }
-        }
-        total = _tatal;
+    function dec() external {
+        count -= 1;
     }
 }
